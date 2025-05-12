95fed63c7692c6ca1f501a0e0dddaa431e36050a
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-05-12 16:50:55 +0200

    add-module

--- app.js
+++ app.js
@@ -2,44 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const posts = require("./posts");
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.json({ message: "Server del mio blog" });
 });
-
-const posts = [
-  {
-    titolo: "Post 01",
-    descrizione: "Ciambellone",
-    immagine: "/img/ciambellone.jpeg",
-    tags: ["#dolciincucina", "#mangiandos'impara", "#passionedolci"],
-  },
-  {
-    titolo: "Post 02",
-    descrizione: "Cracker alla barbabietola",
-    immagine: "/img/cracker_barbabietola.jpeg",
-    tags: ["#dolciincucina", "#mangiandos'impara", "#passionedolci"],
-  },
-  {
-    titolo: "Post 03",
-    descrizione: "Pane fritto dolce",
-    immagine: "/img/pane_fritto_dolce.jpeg",
-    tags: ["#dolciincucina", "#mangiandos'impara", "#passionedolci"],
-  },
-  {
-    titolo: "Post 04",
-    descrizione: "Pasta alla barbabietola",
-    immagine: "/img/pasta_barbabietola.jpeg",
-    tags: ["#dolciincucina", "#mangiandos'impara", "#passionedolci"],
-  },
-  {
-    titolo: "Post 05",
-    descrizione: "Torta paesana",
-    immagine: "/img/torta_paesana.jpeg",
-    tags: ["#dolciincucina", "#mangiandos'impara", "#passionedolci"],
-  },
-];
 
 app.get("/bacheca", (req, res) => {
   res.json({ posts });
---
+++ posts.js
@@ -0,0 +1,34 @@
+const posts = [
+  {
+    titolo: "Post 01",
+    descrizione: "Ciambellone",
+    immagine: "/img/ciambellone.jpeg",
+    tags: ["#dolciincucina", "#mangiandos'impara", "#passionedolci"],
+  },
+  {
+    titolo: "Post 02",
+    descrizione: "Cracker alla barbabietola",
+    immagine: "/img/cracker_barbabietola.jpeg",
+    tags: ["#dolciincucina", "#mangiandos'impara", "#passionedolci"],
+  },
+  {
+    titolo: "Post 03",
+    descrizione: "Pane fritto dolce",
+    immagine: "/img/pane_fritto_dolce.jpeg",
+    tags: ["#dolciincucina", "#mangiandos'impara", "#passionedolci"],
+  },
+  {
+    titolo: "Post 04",
+    descrizione: "Pasta alla barbabietola",
+    immagine: "/img/pasta_barbabietola.jpeg",
+    tags: ["#dolciincucina", "#mangiandos'impara", "#passionedolci"],
+  },
+  {
+    titolo: "Post 05",
+    descrizione: "Torta paesana",
+    immagine: "/img/torta_paesana.jpeg",
+    tags: ["#dolciincucina", "#mangiandos'impara", "#passionedolci"],
+  },
+];
+
+module.exports = posts;
