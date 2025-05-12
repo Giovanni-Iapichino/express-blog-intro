4eb227561fcc47fb2bb7c4497f2cfdf878cc5a72
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-05-12 15:57:08 +0200

    fix-blog

--- app.js
+++ app.js
@@ -5,42 +5,46 @@ const port = 3000;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  console.log("Server del mio blog");
+  res.json({ message: "Server del mio blog" });
 });
 
 const posts = [
   {
-    titolo: "",
-    descrizione: "",
-    immagine: "",
-    tags: [""],
+    titolo: "Post 01",
+    descrizione: "Ciambellone",
+    immagine: "/img/ciambellone.jpeg",
+    tags: ["#dolciincucina", "#mangiandos'impara", "#passionedolci"],
   },
   {
-    titolo: "",
-    descrizione: "",
-    immagine: "",
-    tags: [""],
+    titolo: "Post 02",
+    descrizione: "Cracker alla barbabietola",
+    immagine: "/img/cracker_barbabietola.jpeg",
+    tags: ["#dolciincucina", "#mangiandos'impara", "#passionedolci"],
   },
   {
-    titolo: "",
-    descrizione: "",
-    immagine: "",
-    tags: [""],
+    titolo: "Post 03",
+    descrizione: "Pane fritto dolce",
+    immagine: "/img/pane_fritto_dolce.jpeg",
+    tags: ["#dolciincucina", "#mangiandos'impara", "#passionedolci"],
   },
   {
-    titolo: "",
-    descrizione: "",
-    immagine: "",
-    tags: [""],
+    titolo: "Post 04",
+    descrizione: "Pasta alla barbabietola",
+    immagine: "/img/pasta_barbabietola.jpeg",
+    tags: ["#dolciincucina", "#mangiandos'impara", "#passionedolci"],
   },
   {
-    titolo: "",
-    descrizione: "",
-    immagine: "",
-    tags: [""],
+    titolo: "Post 05",
+    descrizione: "Torta paesana",
+    immagine: "/img/torta_paesana.jpeg",
+    tags: ["#dolciincucina", "#mangiandos'impara", "#passionedolci"],
   },
 ];
 
 app.get("/bacheca", (req, res) => {
-  res.json(posts);
+  res.json({ posts });
+});
+
+app.listen(port, () => {
+  console.log(`Server attivo su http://localhost: ` + port);
 });
