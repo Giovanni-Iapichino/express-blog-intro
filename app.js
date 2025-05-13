b18e27a9c2502697fbd69a887f8dc817cdbd2ccc
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-05-13 12:49:33 +0200

    add-route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const posts = require("./posts");
+const posts = require("./db");
 
 app.use(express.static("public"));
 
